077fd0e0ef5c9009109be3e4069f6fa0a4b73915
uznick <u@uznick.com>
2014-12-18 11:44:07 +0300

    migration

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='django-taggit',
-    version='0.12.2.2t',
+    version='0.12.2.3t',
     description='django-taggit is a reusable Django application for simple tagging.',
     long_description=readme,
     author='Alex Gaynor',
